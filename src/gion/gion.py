2afba32618b70e9682667f21b0897195322d8148
Egor <34124178+osiaeg@users.noreply.github.com>
2025-05-14 16:48:30 +0300

    Revert "Differrent function form Pion"

--- src/gion/gion.py
+++ src/gion/gion.py
@@ -1,23 +1,5 @@
 from pion import Pion
 
-
 class Gion(Pion):
-    def arm(self) -> None:
-        raise NotImplementedError(
-            f"{__class__.__name__} can't implement this function."
-        )
-
-    def disarm(self) -> None:
-        raise NotImplementedError(
-            f"{__class__.__name__} can't implement this function."
-        )
-
-    def takeoff(self) -> None:
-        raise NotImplementedError(
-            f"{__class__.__name__} can't implement this function."
-        )
-
-    def land(self) -> None:
-        raise NotImplementedError(
-            f"{__class__.__name__} can't implement this function."
-        )
+    def __init__(self):
+        pass
