0d4692564f2d86e9e9821611301d7189567d7424
Egor <egor.goncharov.osia@mail.ru>
2025-05-14 16:26:46 +0300

    Make Gion method from Pion NotImplemented

--- src/gion/gion.py
+++ src/gion/gion.py
@@ -1,5 +1,23 @@
 from pion import Pion
 
+
 class Gion(Pion):
-    def __init__(self):
-        pass
+    def arm(self) -> None:
+        raise NotImplementedError(
+            f"{__class__.__name__} can't implement this function."
+        )
+
+    def disarm(self) -> None:
+        raise NotImplementedError(
+            f"{__class__.__name__} can't implement this function."
+        )
+
+    def takeoff(self) -> None:
+        raise NotImplementedError(
+            f"{__class__.__name__} can't implement this function."
+        )
+
+    def land(self) -> None:
+        raise NotImplementedError(
+            f"{__class__.__name__} can't implement this function."
+        )
