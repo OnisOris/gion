65774ab3f03690c51c0670c74fc45eb4ec7317a2
Michael <onisoris@yandex.ru>
2025-08-05 18:59:05 +0300

    Fix: error with t_speed (2)

--- src/gion/gion.py
+++ src/gion/gion.py
@@ -23,7 +23,7 @@ class SwarmCommunicatorGion(SwarmCommunicator):
 
         if self.mode == 3:
             self.control_object.t_speed *= 0.9
-        if np.linalg.norm(self.t_speed) < 0.02:
+        if np.linalg.norm(self.control_object.t_speed) < 0.02:
             self.control_object.t_speed = np.zeros_like(self.control_object.t_speed)
 
         if state.target_id:
